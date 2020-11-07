fb33bf58a5ccdb9ee99755be8dd6578cd6d73d92
NewbieMcCoder <Mquinter1988@gmail.com>
2020-11-06 22:53:50 -0500

    :+1:

--- Terminal Hacker/Assets/Scripts/Hacker.cs
+++ Terminal Hacker/Assets/Scripts/Hacker.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
+    int level;
+    enum Screen { MainMenu, Password, Win };
+    Screen currentScreen;
+
     private void Start()
     {
         ShowMainMenu();
@@ -11,14 +15,12 @@ public class Hacker : MonoBehaviour
 
     private void ShowMainMenu()
     {
+        currentScreen = Screen.MainMenu;
         Terminal.ClearScreen();
-        Terminal.WriteLine("Whatcha want to hack into?");
-        Terminal.WriteLine("");
-        Terminal.WriteLine("Press 1 to hack into your local library");
-        Terminal.WriteLine("Press 2 to hack into your local police station");
-        Terminal.WriteLine("");
-        Terminal.WriteLine("Enter your selection:");
-        Terminal.WriteLine("");
+        Terminal.WriteLine("Whatcha want to hack into?\n");
+        Terminal.WriteLine("Press 1 to hack into your local library\n");
+        Terminal.WriteLine("Press 2 to hack into your local police station\n");
+        Terminal.WriteLine("Enter your selection:\n");
     }
 
     private void OnUserInput(string input)
@@ -27,9 +29,33 @@ public class Hacker : MonoBehaviour
         {
             ShowMainMenu();
         }
+        else if (currentScreen == Screen.MainMenu)
+        {
+            RunMainMenu(input);
+        }
+    }
+
+    private void RunMainMenu(string input)
+    {
+        if (input == "1")
+        {
+            level = 1;
+            StartGame();
+        }
+        else if (input == "2")
+        {
+            level = 2;
+            StartGame();
+        }
         else
         {
             Terminal.WriteLine("Please choose a valid level.");
         }
+    }
+
+    private void StartGame()
+    {
+        currentScreen = Screen.Password;
+        Terminal.WriteLine("You have chosen level " + level);
     }
 }
