65434b6224a9ed65efba8d4f876ed549cdc5f7e1
NewbieMcCoder <Mquinter1988@gmail.com>
2020-11-06 22:35:22 -0500

    :+1:

---
+++ Terminal Hacker/Assets/Scripts/Hacker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    private void Start()
+    {
+        ShowMainMenu();
+    }
+
+    private void ShowMainMenu()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Whatcha want to hack into?");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Press 1 to hack into your local library");
+        Terminal.WriteLine("Press 2 to hack into your local police station");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Enter your selection:");
+        Terminal.WriteLine("");
+    }
+
+    private void OnUserInput(string input)
+    {
+        if (input == "menu")
+        {
+            ShowMainMenu();
+        }
+        else
+        {
+            Terminal.WriteLine("Please choose a valid level.");
+        }
+    }
+}
